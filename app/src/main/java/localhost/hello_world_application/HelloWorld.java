c59485b1970713961e2c0b71cfe64f6154ea778f
Mika Vallin <mika.vallin@gmail.com>
2018-11-21 00:02:31 +0200

    Created new project in Android Studio
    All default files from new Android Studio project.

---
+++ app/src/main/java/localhost/hello_world_application/HelloWorld.java
@@ -0,0 +1,13 @@
+package localhost.hello_world_application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloWorld extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_world);
+    }
+}
